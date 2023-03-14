06d0dafb0c6ca056acfae14ab46be939531955dd
peikui <peikui@canalasset.com>
2023-03-14 14:17:48 +0800

    update

--- contracts/utils/interfaces/ITreasury.sol
+++ contracts/utils/interfaces/ITreasury.sol
@@ -3,9 +3,6 @@
 pragma solidity 0.8.13;
 
 interface ITreasury {
-    function burn(uint256 amount) external;
-
-    function burnFrom(address from, uint256 amount) external;
 
     function isOperator() external returns (bool);
 
@@ -14,20 +11,11 @@ interface ITreasury {
     function transferOperator(address newOperator_) external;
 
     function period() external view returns (uint256);
-    
+
     function epoch() external view returns (uint256);
 
     function nextEpochPoint() external view returns (uint256);
 
-    function getTokenPrice() external view returns (uint256);
-
-    function getRealtimeTokenIndexPrice() external view returns (uint256);
-
     function initialize(address token, address share, address oracle, address boardroom, uint256 start_time) external;
 
-    function allocateSeigniorage() external;
-
-    function rebase() external returns (bool);
-
-    function setInitialPrice(uint256 _initialEpochTokenIndexPrice) external returns (bool);
 }
